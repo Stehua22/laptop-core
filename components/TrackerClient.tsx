09b3f6cf48c98c00ee6e72bb674e271e645e5b1c
SteHua22 <stevechick2@gmail.com>
2026-06-06 20:29:35 -0400

    feat: redesign tracker list layout

--- components/TrackerClient.tsx
+++ components/TrackerClient.tsx
@@ -218,7 +218,6 @@ export default function TrackerClient({ initialLaptops, dbError }: { initialLapt
             ⚠ {dbError}
           </div>
         )}
-        <Header onAdd={handleAddClick} isDark={isDark} onThemeToggle={() => set
         <StatsBar stats={stats} currency={currency} cadToUsd={cadToUsd} />
         
         <Controls search={search} onSearch={setSearch} brands={brands} brandFilter={brandFilter} onBrandFilter={setBrandFilter} sortBy={sortBy} onSort={setSortBy} />
