8b686e246e144acf06caaa4f1c69dc46d2e772db
SteHua22 <stevechick2@gmail.com>
2026-08-04 22:08:19 -0400

    Add Deal Scanner to sidebar nav

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,4 @@
-﻿import { useState, ReactNode } from "react";
+import { useState, ReactNode } from "react";
 import styles from "./Sidebar.module.css";
 
 type NavItem = {
@@ -81,6 +81,17 @@ const NAV_ITEMS: NavItem[] = [
     icon: (
       <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
         <path d="M12 3C7 3 3 7 3 12s4 9 9 9 9-4 9-9-4-9-9-9zm-1 14H9V7h2v10zm4-4h-2V7h2v6z"/>
+      </svg>
+    ),
+  },
+  {
+    key: "deal-scanner",
+    label: "Deal Scanner",
+    href: "/deals/scanner",
+    icon: (
+      <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 14H9V8h2v8zm4-4h-2V8h2v4z"/>
+        <path d="M11 2v2a8 8 0 017.43 5H21.9A10 10 0 0011 2zM21.9 15H19.4A8 8 0 0113 21.9V22a10 10 0 008.9-7z" opacity=".5"/>
       </svg>
     ),
   },
